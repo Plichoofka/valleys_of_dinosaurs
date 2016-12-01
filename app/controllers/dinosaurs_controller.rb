312b87fe55823dca8b3c605ed46361108be3b54a
Anna Plich <aplich86@gmail.com>
2016-12-01 21:58:22 +0100

    Views and Controllers: Show and New

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,5 +1,29 @@
 class DinosaursController < ApplicationController
+# action for the main page
   def index
     @dinosaurs = Dinosaur.all
   end
+#action for showing the single dino page
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+# ???"new" action method for creating a new dino (instance/object)
+  def new
+   @dinosaur = Dinosaur.new
+ end
+
+# action for populating a new record
+ def create
+    dinosaur_params = params.require( :dinosaur ).permit( :name, :age, :image_url )
+
+    @dinosaur = Dinosaur.new( dinosaur_params )
+
+    if @dinosaur.save
+       redirect_to @dinosaur
+    else
+       render 'new'
+    end
+  end
+
 end
