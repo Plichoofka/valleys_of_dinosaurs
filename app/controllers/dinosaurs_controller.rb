1dca7a72ace8f55577271109623edd0277017e88
Anna Plich <aplich86@gmail.com>
2016-12-01 22:47:04 +0100

    Views and Controllers:Edit,Update and Delete

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -3,6 +3,7 @@ class DinosaursController < ApplicationController
   def index
     @dinosaurs = Dinosaur.all
   end
+
 #action for showing the single dino page
   def show
     @dinosaur = Dinosaur.find(params[:id])
@@ -25,5 +26,40 @@ class DinosaursController < ApplicationController
        render 'new'
     end
   end
+# # method for editing an already existing record
+  def edit
+  @dinosaur = Dinosaur.find(params[:id])
+end
+
+# method to update a single dinosaur
+
+def update
+  @dinosaur = Dinosaur.find( params[:id] )
+
+  dinosaur_params = params.require( :dinosaur ).permit( :name, :age, :image_url )
+
+  if @dinosaur.update_attributes( dinosaur_params )
+     redirect_to @dinosaur
+  else
+     render 'edit'
+  end
+end
+
+# # Refactor the common code for dinosaur params into a private method.
+# # squeezing 2 loops into one
+#
+# private
+#
+#   def dinosaur_params
+#     params.require(:dinosaur).permit(:name, :age, :image_url)
+#   end
+# mathod for deleting a dinosaur
+def destroy
+   @dinosaur = Dinosaur.find(params[:id])
+
+   @dinosaur.destroy
+
+   redirect_to dinosaurs_path
+ end
 
 end
