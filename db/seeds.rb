cff61790cf05c7fc5eaa707d43972bc7ceb7e944
Anna Plich <aplich86@gmail.com>
2016-12-01 20:13:22 +0100

    Views and Controllers: Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 dino1 = Dinosaur.create( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 =  Dinosaur.create( { name: "Random1", age: rand(1..100), image_url:  } )
+dino5 =  Dinosaur.create( { name: "Random2", age: rand(1..100), image_url:  } )
